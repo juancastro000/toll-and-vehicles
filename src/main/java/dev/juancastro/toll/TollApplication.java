134a9ce7b28cf6111984515b4f62e295ae91d094
juancastro000 <cdyutub@gmail.com>
2025-03-25 21:28:54 +0100

    first commit

---
+++ src/main/java/dev/juancastro/toll/TollApplication.java
@@ -0,0 +1,13 @@
+package dev.juancastro.toll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/juancastro/toll/TollApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.juancastro.toll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
