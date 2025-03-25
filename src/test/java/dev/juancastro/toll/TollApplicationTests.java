227101035d54783b49e9389dd56c1c372404c483
juancastro000 <cdyutub@gmail.com>
2025-03-25 22:50:37 +0100

    test red print report

--- src/test/java/dev/juancastro/toll/TollApplicationTests.java
+++ src/test/java/dev/juancastro/toll/TollApplicationTests.java
@@ -1,5 +1,8 @@
 package dev.juancastro.toll;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +10,29 @@ import org.springframework.boot.test.context.SpringBootTest;
 class TollApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
+    void main_execution_shouldPrintCorrectReport() {
+        PrintStream originalOut = System.out;
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(bos));
+
+        try {
+            TollApplication.main(new String[0]);
+            String output = bos.toString();
+            assertThat(output, stringContainsInOrder(
+                "=== REPORTE DE PEAJE ===",
+                "Estación: PEAJE VIVA",
+                "Ubicación: Medellín",
+                "Vehículos procesados:",
+                "ABC123 - $100",
+                "XYZ789 - $50",
+                "TRK456 - $150",
+                "DEF456 - $100",
+                "CAM123 - $250",
+                "Total recolectado: $650"
+            ));
+        } finally {
+            System.setOut(originalOut);
+        }
+    }
 
 }
